2c71c7509c31bfd74ab4a0656cedfe3d2a471564
Akshay <akshay_h_G@hotmail.com>
2022-06-26 03:07:30 +0530

    message

--- src/test/java/rough/InnerClass.java
+++ src/test/java/rough/InnerClass.java
@@ -1,2 +1,20 @@
-package rough;public class InnerClass {
+package rough;
+
+public class InnerClass {
+    public class Inner{
+        int y = 20;
+        void display(){
+            System.out.println(y);
+        }
+    }
+    void outerDisplay(){
+        Inner i=new Inner();
+        i.display();
+    }
+
+    public static void main(String[] args) {
+        Inner in = new InnerClass().new Inner();
+        InnerClass ic = new InnerClass();
+        ic.outerDisplay();
+    }
 }
--- src/test/java/rough/Student.java
+++ src/test/java/rough/Student.java
@@ -1,2 +1,28 @@
-package rough;public class Student {
+package rough;
+
+import java.util.Date;
+
+public class Student {
+    private String rollNo = "Univ-2020-";
+    private static int no = 1;
+    private Student(){
+        rollNo = generateRollNo();
+    }
+
+    private String generateRollNo() {
+        Date d = new Date();
+        String rn = "Unvi-"+d.getTime()+"-"+no++;
+        return rn;
+    }
+
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        Student st2 = new Student();
+        Student st3 = new Student();
+        Student st4 = new Student();
+        System.out.println(st1.rollNo);
+        System.out.println(st2.rollNo);
+        System.out.println(st3.rollNo);
+        System.out.println(st4.rollNo);
+    }
 }
