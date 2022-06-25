ee89af88e389477a20123db3a378809a0a3ba359
Akshay <akshay_h_G@hotmail.com>
2022-06-26 03:58:55 +0530

    branch name update

--- src/test/java/rough/CofeeMachine.java
+++ src/test/java/rough/CofeeMachine.java
@@ -1,2 +1,26 @@
-package rough;public class CofeeMachine {
+package rough;
+
+public class CofeeMachine {
+    private float cofeeQty;
+    private float watQty;
+    private static CofeeMachine cfm = null;
+    private CofeeMachine(){
+        cofeeQty = 1;
+        watQty = 1;
+    }
+    static public CofeeMachine getCofeeMachineInstance(){
+        if (cfm==null)
+            cfm = new CofeeMachine();
+        return cfm;
+    }
+
+    public static void main(String[] args) {
+        CofeeMachine cf = getCofeeMachineInstance();
+        CofeeMachine cf1 = getCofeeMachineInstance();
+        CofeeMachine cf2 = getCofeeMachineInstance();
+        System.out.println(cf);
+        System.out.println(cf1);
+        System.out.println(cf2);
+
+    }
 }
